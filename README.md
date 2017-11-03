45c68043530b4dd9b3f5ad967aced38d673dbacf
pauloXtr3m <paulo-gyn@outlook.com>
2017-11-03 18:26:11 -0200

    add verification if params are empty

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ O Projeto será executado pelos seguintes alunos:
 3. Paulo de Oliveira Neto ([@pauloXtr3m](https://github.com/pauloXtr3m))
 
 ## Setup
+Baixe o NodeJs no seguinte [link](https://www.npmjs.com/get-npm?utm_source=house&utm_medium=homepage&utm_campaign=free%20orgs&utm_term=Install%20npm "Get NPM")
+
 Para baixar as dependências do projeto, rode:
 ```
 $ npm install
@@ -17,4 +19,16 @@ $ npm install
 Para rodar o projeto, execute:
 ```
 $ npm start
+```
+
+## Rotas
+O projeto, por padrão, roda na porta `:3003` e possui as seguintes rotas:
+```
+               Prefix Verb   URI Pattern                           Controller#Action
+               people GET    /api/people(.:format)                 people#list
+               people POST   /api/people(.:format)                 people#create
+               people GET    /api/people/:id                       people#read
+               people PUT    /api/people/:id(.:format)             people#update
+               people DELETE /api/people/:id(.:format)             people#destroy
+                 
 ```
--- api/person/personService.js
+++ api/person/personService.js
@@ -13,20 +13,27 @@ exports.listPeople = (req, res) =>{
 exports.createPerson = (req, res) =>{
     const newPerson = new Person(req.body)
 
-    Person.findOneAndUpdate(
-        {registry_number: newPerson.registry_number},
-        {name: newPerson.name, birth: newPerson.birth},
-        {upsert: true},
+    if(isEmpty(newPerson.registry_number)
+        || isEmpty(newPerson.name)){
+        res.json(null)
+    } else {
+        Person.findOneAndUpdate(
+            {registry_number: newPerson.registry_number},
+            {name: newPerson.name, birth: newPerson.birth},
+            {upsert: true},
 
-        function(err, person) {
-            if (err)
-                res.send(err)
-            Person.findOne({registry_number: newPerson.registry_number}, function(err, person) {
+            function(err, person) {
                 if (err)
                     res.send(err)
-                res.json(person)
+                Person.findOne({registry_number: newPerson.registry_number}, function(err, person) {
+                    if (err)
+                        res.send(err)
+                    res.json(person)
+                })
             })
-        })
+    }
+
+
 }
 
 exports.readPerson = (req, res) =>{
@@ -38,28 +45,57 @@ exports.readPerson = (req, res) =>{
 }
 
 exports.removePerson = (req, res) =>{
-    Person.findOneAndRemove({registry_number: req.params.registry_number}, function(err, person) {
-        if (err)
-            res.send(err)
-        res.json(person)
-    })
+    if(isEmpty(req.params.registry_number)){
+        res.json(null)
+    } else {
+        Person.findOneAndRemove({registry_number: req.params.registry_number}, function(err, person) {
+            if (err)
+                res.send(err)
+            res.json(person)
+        })
+    }
 }
+
+
 
 exports.updatePerson = (req, res) =>{
     const newPerson = new Person(req.body);
 
-    Person.findOneAndUpdate(
-        {registry_number: req.params.registry_number},
-        {name: newPerson.name, birth: newPerson.birth},
-        {upsert: true},
+    if(isEmpty(req.params.registry_number)){
+        res.json(null)
+    } else {
+        Person.findOneAndUpdate(
+            {registry_number: req.params.registry_number},
+            chooseAttr(newPerson),
+            {upsert: true},
 
-        function(err, person) {
-            if (err)
-                res.send(err)
-            Person.findOne({registry_number: newPerson.registry_number}, function(err, person) {
+            function(err, person) {
                 if (err)
                     res.send(err)
-                res.json(person)
-            })
-        })
+                Person.findOne({registry_number: newPerson.registry_number}, function(err, person) {
+                    if (err)
+                        res.send(err)
+                    res.json(person)
+                })
+            }
+        )
+    }
 }
+
+const chooseAttr = (person) => {
+    if(isEmpty(person.name) && isEmpty(person.birth)){
+        return {}
+    } else if(isEmpty(person.name)) {
+        return {birth: person.birth}
+    } else if(isEmpty(person.birth)) {
+        return {birth: person.name}
+    } else{
+        return {name: person.name, birth: person.birth}
+    }
+}
+
+const isEmpty = (attr) => {
+    if(attr === "") return true
+    else return false
+}
+
